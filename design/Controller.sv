c7b27d1652284f75b6b89b324bd6580c13e30457
Álvaro <145587864+MonstroHormonal@users.noreply.github.com>
2024-07-29 22:35:46 -0300

    Update Controller.sv
    Branch implementado

--- design/Controller.sv
+++ design/Controller.sv
@@ -16,23 +16,31 @@ module Controller (
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic JalSel,
+    output logic JalrSel
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, RI_TYPE, LW, SW, BR, LUI, JAL, JALr;
 
   assign R_TYPE = 7'b0110011;  //add,and
-  assign I_TYPE = 7'b0010011; //addi
+  assign RI_TYPE = 7'b0010011; //addi
   assign LW = 7'b0000011;  //lw
   assign SW = 7'b0100011;  //sw
   assign BR = 7'b1100011;  //beq
+  assign LUI = 7'b0110111;  //lui
+  assign JAL = 7'b1101111; //JAL
+  assign JALr = 7'b1100111; //JALr
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE);
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == RI_TYPE || Opcode == LUI); 
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == RI_TYPE || Opcode == LUI || Opcode == JAL || Opcode == JALr); //|| Opcode == JAL
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR);
-  assign ALUOp[1] = (Opcode == R_TYPE) || (Opcode == I_TYPE);
-  assign Branch = (Opcode == BR);
+  assign ALUOp[0] = (Opcode == BR || Opcode == LUI || Opcode == JAL || Opcode == JALr);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == RI_TYPE || Opcode == LUI);
+  assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALr);
+  assign JalSel = (Opcode == JAL || Opcode == JALr);
+  assign JalrSel = (Opcode == JALr);
+
 endmodule
